5a5e9328939d3df98c210e16455482d3b4a0ed6e
yangboz <YoungWelle@gmail.com>
2014-10-20 20:22:29 +0800

    ADDED:stompjs for websocket test, crossdomain.xml/web.xml for Jetty9 allow cross domain filter

--- activiti-websocket-client-js-angular/js/main.js
+++ activiti-websocket-client-js-angular/js/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+//@:http://stackoverflow.com/questions/8303162/jetty-cross-origin-filter
 //    ws = new WebSocket("ws://" + location.hostname + ":9080/");
-    ws = new WebSocket("ws://127.0.0.1:9080");
-//    ws = new WebSocket("ws://echo.websocket.org");
+//    ws = new WebSocket("ws://0.0.0.0:61614");
+    ws = new WebSocket("ws://echo.websocket.org");
 
     ws.onmessage = function(event) {
         $("#messages").append("<p>" + event.data + "</p>");
     };
 
-    ws.onclose = function() {
-        console.log("Socket closed");
-    };
+    ws.onclose = function(event) {
+        console.log("Socket closed:",event);
+    }
+
+    ws.onerror = function(error) {
+        console.log("Socket error:",error);
+    }
 
     ws.onopen = function() {
         console.log("Connected");
